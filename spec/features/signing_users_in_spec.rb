a0950e54d71025e27bf7d78610005280a9ef2324
icinine <shane.carr@sitesignals.com>
2015-12-09 22:58:17 +0000

    user signin

---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.feature "Users signin" do
+  before do
+  @bob = User.create!(email: "bob@example.com", password: "password")
+  end
+ 
+  scenario "with valid credentials" do
+    visit "/"
+    click_link "Sign in"
+    
+    fill_in "Email", with: @bob.email
+    fill_in "Password", with: @bob.password
+    click_button "Log in"
+    
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("Signed in as #{@bob.email}")
+    
+    
+    
+  end
+  
+  
+end
