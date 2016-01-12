359d24c990bb78e5cb757eec44c2c910a42ff5b1
icinine <shane.carr@sitesignals.com>
2016-01-12 22:00:25 +0000

    Implement displat friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,10 @@
-class FriendshipsController < ApplicationsController
+class FriendshipsController < ApplicationController
   
   before_action :authenticate_user!
   
   def create
     friend = User.find(params[:friend_id])
-    friendship.create(friendship_params.merge!(friend_id: params[:friiend_id], user_id: current_user.id)) unless current_user.follows_or_same?(friend)
+    Friendship.create(friendship_params.merge!(friend_id: params[:friend_id], user_id: current_user.id)) unless current_user.follows_or_same?(friend)
     redirect_to root_path
   end
   
--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.feature "Listing exercises" do
   before do
     @john = User.create(first_name: "Bob", last_name: "Doe", email: "john@examples.com", password: "password")
+    @sara = User.create(first_name: "Sara", last_name: "Doe", email: "sara@examples.com", password: "password")
     login_as(@john)
     
     @e1 = @john.exercises.create(duration_in_min: 50, workout: "Squats" , workout_date: Date.today) 
     @e2 = @john.exercises.create(duration_in_min: 60, workout: "Cardio", workout_date: Date.today) 
+ 
+    @following = Friendship.create(user: @john, friend: @sara)
+  
   end
   
   scenario "shows users workouts for last 7 days" do
@@ -20,6 +24,18 @@ RSpec.feature "Listing exercises" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
+    
+  end
+  
+  
+  scenario "shows a list of users friends" do
+    visit "/"
+    
+    click_link "My Lounge"
+    expect(page).to have_content("My Friends")
+    expect(page).to have_link(@sara.full_name)
+    expect(page).to have_link("Unfollow")
+    
     
   end
 end
