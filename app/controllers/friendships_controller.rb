d81e5c49a847839d1ec286acbf6079e95b6b9327
icinine <shane.carr@sitesignals.com>
2016-01-13 21:22:59 +0000

    Friendship Controller update

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,14 @@
 class FriendshipsController < ApplicationController
-  
   before_action :authenticate_user!
   
   def create
     friend = User.find(params[:friend_id])
-    Friendship.create(friendship_params.merge!(friend_id: params[:friend_id], user_id: current_user.id)) unless current_user.follows_or_same?(friend)
+    Friendship.create(friendship_params.merge!(friend_id: params[:friend_id], 
+      user_id: current_user.id)) unless current_user.follows_or_same?(friend)
     redirect_to root_path
   end
   
-  def show 
+  def show
     @friend = Friendship.find(params[:id]).friend
     @exercises = @friend.exercises.all
   end
@@ -23,8 +23,7 @@ class FriendshipsController < ApplicationController
     redirect_to user_exercises_path(current_user)
   end
   
-  
-  private
+  private 
   
   def friendship_params
     params.permit(:friend_id, :user_id)
